df2875c8f787ff86cd3d793d3ca5babc0849a5cf
Nathan Alex <nate745@gmail.com>
2019-10-05 23:57:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def long_planeteer_calls(call_array)
   call_array.any? {|item| item.length >4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
