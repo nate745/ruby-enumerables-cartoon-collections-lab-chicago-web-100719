7dd384e082397c99a3fa4b54d8ffa68a1e376c27
Nathan Alex <nate745@gmail.com>
2019-10-05 23:55:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(call_array)
   call_array.map {|item| "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_array)
+  call_array.select {|item| item.length >4}
 end
 
 def find_the_cheese# code an argument here
